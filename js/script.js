0d97ad45cb47fa50323624ce3c74f4baf4f30111
SamuelNdreu <samwellsoficcial@gmail.com>
2022-12-06 17:58:40 +0100

    maybe finished? who knows

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// Consegna:
+// Modifichiamo il codice dell'esercizio per renderlo funzionante con
+// un array di oggetti, con una regola: non potete modificare l'array di oggetti in nessun caso.
+// Dato un array di oggetti letterali con:
+//  url dell’immagine
+// titolo
+// descrizione
+// Creare un carosello.
+// Milestone 0:
+// Popoliamo dinamicamente il contenuto del carosello con i dati forniti dall'array di oggetti,
+// dal js (dentro al nostro 'carousel-item', per capirci).
+// Milestone 1:
+// Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventerà visibile.
+// Milestone 2:
+// Aggiungiamo alla visualizzazione delle immagini anche titolo e testo relative alla singola immagine.
+// Bonus 1:
+// Aggiungere il ciclo infinito del carosello. Ovvero se l'immagine attiva è la prima e l'utente clicca la freccia verso l'alto,
+// l'immagine che deve attivarsi sarà l'ultima e viceversa per l'ultima immagine se l'utente clicca la freccia verso il basso.
+// Bonus 2:
+// Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
+// Bonus 3:
+// Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva
+// dovrà cambiare alla successiva.
+
 const images = [
    {
       image: 'img/01.webp',
@@ -27,16 +51,26 @@ const images = [
 ];
 
 
-const carouselImageEl = document.querySelector(".carousel-image")
-const upButton = document.getElementById('previousb')
-const downButton = document.getElementById('nextb')
+const divCarouselImage = document.querySelector('div.carousel-image');
 
 
-upButton.addEventListener("click", function () {
-   alert("cliccato")
+let divMyCarousel;
+
+const buttonPrevious = document.getElementById('previous');
+const buttonNext = document.getElementById('next');
+
+
+for (let index = 0; index < images.length; index++) {
+   divMyCarousel = document.createElement('div');
+   divMyCarousel.innerHTML = `<img src="./${images[index].image}" alt="image">`
+   divMyCarousel.classList.add('my_carousel-item')
+   divCarouselImage.appendChild(divMyCarousel);
+}
+
+//Aggiungo un evento click al buttone in alto
+buttonPrevious.addEventListener('click', function() {
 });
 
-downButton.addEventListener("click", function () {
-})
-
-
+//Aggiungo un evento click al buttone in basso
+buttonNext.addEventListener('click', function(){
+});
